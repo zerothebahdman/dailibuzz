c6f342fbf474bd5359b74192e9ca8ba0589232c0
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2022-01-09 15:03:24 +0100

    Feature 🏋: getting healines from third party services.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-module.export = app;
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const dotenv = require("dotenv");
-const app = require("./index");
+const dotenv = require('dotenv');
+const app = require('./index');
 
 dotenv.config();
 const port = process.env.PORT;
