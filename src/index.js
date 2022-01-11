a9b58f6fa378335ebd4afcf95084bbde7d703f4c
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2022-01-11 15:28:39 +0100

    Chore ⚙: Refactored Code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 require('./scrapper/punch/news');
 const express = require('express');
+const morgan = require('morgan');
+const AppError = require('./class/AppError');
+const ErrorHandler = require('./middleware/ErrorHandler');
+const articleRouter = require('./routes/article');
 
 const app = express();
+app.use(express.json());
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/article/', articleRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Cant find ${req.originalUrl} on the server.`, 404));
+});
+app.use(ErrorHandler);
 
 module.exports = app;
