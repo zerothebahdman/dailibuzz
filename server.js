f17bad338f4a0c404734ff49b6b8d4122ad1ad43
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2022-01-12 15:29:41 +0100

    Chore ⚙: Handling unhandled exception and rejection.

--- server.js
+++ server.js
@@ -2,10 +2,23 @@ const dotenv = require('dotenv');
 const app = require('./src/index');
 const { sequelize } = require('./models');
 
+process.on('uncaughtException', (err) => {
+  console.log(`Unhandled Exception💣! Shutdown In Progress...`);
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 dotenv.config();
 const port = process.env.PORT;
-app.listen(port, async () => {
+const server = app.listen(port, async () => {
   console.log(`🚀 is running on port ${port}`);
   await sequelize.authenticate();
   console.log('Database connected successfully');
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(`Unhandled Rejection!!💣 Shutdown In Progress`);
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
