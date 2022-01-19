afe5b591bc270d232583082ef088c1e43665ce0d
Divinewill ZERO <zerothread@users.noreply.github.com>
2022-01-19 22:30:11 +0100

    [ Chore ⚙ ] Implemented scrapping for sports aritcles.

--- src/scrapper/punch/sports.js
+++ src/scrapper/punch/sports.js
@@ -3,5 +3,5 @@ const createArticle = require('../utils/createArticle');
 
 cron.schedule('00,08 00,00 9,12 * * *', async () => {
   /**cron will run every day by 9:00AM and 12:00PM */
+  createArticle('https://punchng.com/topics/sports/', 'Sports', 'punchng');
 });
-createArticle('https://punchng.com/topics/sports/', 'Sports', 'punchng');
