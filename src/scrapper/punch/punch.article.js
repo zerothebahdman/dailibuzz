0066755d8b6114af603d3c5d903740f885a9f95b
Divinewill ZERO <zerothread@users.noreply.github.com>
2022-01-20 15:23:26 +0100

    [ Bug 🐞 ] Fixed typo

--- src/scrapper/punch/punch.article.js
+++ src/scrapper/punch/punch.article.js
@@ -1,14 +1,14 @@
 const cron = require('node-cron');
-const createArticle = require('../utils/createArticle');
+const { punchArticle } = require('../utils/createArticle');
 
 cron.schedule('00 00,00 8,12 * * *', async () => {
   /**cron will run every day by 8:00AM and 12:00PM */
-  createArticle(
+  punchArticle(
     'https://punchng.com/topics/entertainment/',
     'Entertainment',
     'punchng'
   );
-  createArticle('https://punchng.com/topics/news/', 'News', 'punchng');
-  createArticle('https://punchng.com/topics/business/', 'Business', 'punchng');
-  createArticle('https://punchng.com/topics/sports/', 'Sports', 'punchng');
+  punchArticle('https://punchng.com/topics/news/', 'News', 'punchng');
+  punchArticle('https://punchng.com/topics/business/', 'Business', 'punchng');
+  punchArticle('https://punchng.com/topics/sports/', 'Sports', 'punchng');
 });
