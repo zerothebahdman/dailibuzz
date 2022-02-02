7d91c6ad145adf18944188666aad769b30e3e653
Divinewill ZERO <zerothread@users.noreply.github.com>
2022-02-02 21:48:46 +0100

    [ Feature 🚀 ] started implementation of nestjs version.

--- src/scrapper/tech-crunch/tech-crunch.article.js
+++
@@ -1,6 +0,0 @@
-const cron = require('node-cron');
-const { techCrunchArticles } = require('../utils/createArticle');
-
-cron.schedule('00 00,00 8,12 * * *', async () => {
-  techCrunchArticles('https://techcrunch.com/', 'Tech', 'tech-crunch');
-});
