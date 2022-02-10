b3abc235566e1d4fd2699524d1de16815c26557b
Divinewill ZERO <codewithdiv@users.noreply.github.com>
2022-02-10 23:35:42 +0100

    [ Feature 🚀 ] Successfully saved articles to the database.

---
+++ prisma/migrations/20220210211408_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `uuid` on the `article` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `article` DROP COLUMN `uuid`;
---
+++ prisma/migrations/20220210222710_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `uuid` on the `category` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `category` DROP COLUMN `uuid`;
