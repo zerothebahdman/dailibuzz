98cd7c90c6b2511c15703a8b53bd8db3680a76fd
Divinewill ZERO <codewithdiv@users.noreply.github.com>
2022-03-17 14:21:32 +0100

    [Feature 🚀] Added techcrunch to article service

--- src/scrapper/service/Article.service.ts
+++ src/scrapper/service/Article.service.ts
@@ -1,9 +1,24 @@
 import PunchScrapperClass from '../Class/PunchScrapperClass';
 import log from '../../logging/logger';
+import TechCrunchArticleScrapper from '../Class/TechCrunchScrapperClass';
 
 export default class ArticleService {
   async punchArticle(url: string, category: string, source: string) {
     const article = new PunchScrapperClass(url, category, source);
+    article
+      .getArticle()
+      .then((results) => {
+        log.info(`Number of results ${results.length}`);
+        article.exportResults(results, category, source);
+        log.info(results);
+      })
+      .catch((err) => {
+        log.info(`Error while fetching articles with error :::: ${err}`);
+      });
+  }
+
+  async techCrunchArticle(url: string, category: string, source: string) {
+    const article = new TechCrunchArticleScrapper(url, category, source);
     article
       .getArticle()
       .then((results) => {
