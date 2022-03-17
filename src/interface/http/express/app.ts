edc1729a3eca38edec53a29d5a8b7ca35087c74e
Divinewill ZERO <codewithdiv@users.noreply.github.com>
2022-03-17 12:40:12 +0100

    [Chore ⚙] imported article endpoints into app.ts

--- src/interface/http/express/app.ts
+++ src/interface/http/express/app.ts
@@ -1,11 +1,15 @@
+import '../../../scrapper/article/PunchArticleScrapper';
+// import '../../../scrapper/article/TechCrunchArticleScrapper';
 import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import config from 'config';
 import enforce from 'express-sslify';
-import userRouter from './router/user/user.route';
+import userRouter from './router/user.route';
 import ErrorHandler from './middlewares/error_handler.middleware';
 import AppException from '../../../exceptions/AppException';
 import morgan from 'morgan';
+import categoryRouter from './router/category.router';
+import articleRouter from './router/article.route';
 
 const app: Application = express();
 
@@ -22,6 +26,8 @@ app.use(cors());
 app.disable('x-powered-by');
 
 app.use('/api/v1/user', userRouter);
+app.use('/api/v1/article/', articleRouter);
+app.use('/api/v1/category/', categoryRouter);
 
 app.all('*', (req: Request, res: Response, next: NextFunction) => {
   return next(
